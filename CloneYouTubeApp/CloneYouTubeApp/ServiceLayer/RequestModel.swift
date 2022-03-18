f62ab731b1bb29ac3fdec35492f07733be82255f
ivan <ivanvalero1985@gmail.com>
2022-03-18 18:19:19 -0300

    Servicio de red

--- CloneYouTubeApp/CloneYouTubeApp/ServiceLayer/RequestModel.swift
+++ CloneYouTubeApp/CloneYouTubeApp/ServiceLayer/RequestModel.swift
@@ -6,3 +6,32 @@
 //
 
 import Foundation
+
+
+struct RequestModel {
+    let endpoint: Endpoints
+    var queryItems: [String:String]?
+    let httpMethod: HttpMethod = .GET
+    var baseUrl: URLBase = .youtube
+    
+    func getURL() -> String {
+        return baseUrl.rawValue + endpoint.rawValue
+    }
+    
+    enum HttpMethod: String {
+        case GET
+        case POST
+    }
+    
+    enum Endpoints: String {
+        case search = "/search"
+        case empty = ""
+    }
+    
+    enum URLBase: String {
+        case youtube = "https://www.googleapis.com/youtube/v3"
+        case google = "https://otherweb.com/v2"
+    }
+    
+}
+
